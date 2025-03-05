37630caede4e5201ad77367cdc46ae73dedac09c
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-03-05 20:41:53 -0300

    Course completed!

--- machine_learning.py
+++ machine_learning.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 
 class MachineLearning:
@@ -34,17 +36,17 @@ class MachineLearning:
         return data[train_id], data[test_id], target[train_id], target[test_id]
 
     @staticmethod
-    def get_decision_tree_classifier() -> DecisionTreeClassifier:
+    def get_decision_tree_classifier(**kwargs) -> DecisionTreeClassifier:
         """
         Obtains the tree object for Machine Learning training.
 
         :return: Object ready for training
         """
-        return DecisionTreeClassifier()
+        return DecisionTreeClassifier(**kwargs)
 
     @staticmethod
-    def fit_decision_tree(decision_tree: DecisionTreeClassifier, feature_train: np.ndarray,
-                          target_train: np.ndarray) -> DecisionTreeClassifier:
+    def fit_decision_tree(decision_tree, feature_train: np.ndarray,
+                          target_train: np.ndarray):
         """
         Trains a decision tree object with a given feature and target data.
 
@@ -64,6 +66,70 @@ class MachineLearning:
         :return:
         """
         return decision_tree.predict(X=feature_test)
+
+    @staticmethod
+    def get_tree_score(target_test: np.ndarray, target_predict: DecisionTreeClassifier) -> float | int:
+        """
+        Returns the score of a tree prediction vs. real data
+
+        :param target_test: Array containing the real data for comparison
+        :param target_predict: Tree object with the predicted data
+        :return: Float or Int of the accuracy of the comparison
+        """
+        return accuracy_score(target_test, target_predict)
+
+    @staticmethod
+    def get_confusion_matrix(target_test: np.ndarray, target_predict: DecisionTreeClassifier):
+        """
+        Returns the matrix between the comparison of a trained model prediction and the real data
+
+        :param target_test: Array containing the real data for comparison
+        :param target_predict: Tree object with the predicted data
+        :return:
+        """
+        return confusion_matrix(y_true=target_test, y_pred=target_predict)
+
+    @staticmethod
+    def get_precision_score(target_test: np.ndarray, target_predict: DecisionTreeClassifier):
+        """
+        Calculates the precision score of a prediction over a test data
+
+        :param target_test: Test data for comparison
+        :param target_predict: Trained Decision Tree
+        :return: Calculated precision score
+        """
+        return precision_score(y_true=target_test, y_pred=target_predict)
+
+    @staticmethod
+    def get_recall_score(target_test: np.ndarray, target_predict: DecisionTreeClassifier):
+        """
+        Calculates the recall score of a prediction over a test data
+
+        :param target_test: Test data for comparison
+        :param target_predict: Trained Decision Tree
+        :return: Calculated recall score
+        """
+        return recall_score(y_true=target_test, y_pred=target_predict)
+
+    @staticmethod
+    def get_random_forest_classifier(**kwargs) -> RandomForestClassifier:
+        """
+        Initiates an instance of a Random Forest Classifier
+
+        :param kwargs:
+        :return: Instance of Random Forest Classifier
+        """
+        return RandomForestClassifier(**kwargs)
+
+    @staticmethod
+    def get_ada_boost_classifier(**kwargs):
+        """
+        Initiates an instance of Ada Boost Classifier
+
+        :param kwargs:
+        :return: Instance of Ada Boost Classifier
+        """
+        return AdaBoostClassifier(**kwargs)
 
     @staticmethod
     def save_tree_plot(decision_tree: DecisionTreeClassifier, name: str, **kwargs) -> None:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from machine_learning import MachineLearning
-#from data_analysis import FraudAnalysis
+from data_analysis import FraudAnalysis
 
 
 class Main:
@@ -9,12 +9,12 @@ class Main:
 
 
     def process(self):
-        # fa = FraudAnalysis(data=self.data)
-        # print(f"Number of rows of the dataset: {fa.get_total_rows()}")
-        # print(f"Number of frauds present in the dataset: {fa.get_total_fraud('Class')}")
-        # print(f"Number of non-frauds present in the dataset: {fa.get_total_non_fraud('Class')}")
-        # print(f"Percentage of frauds present in the dataset: {fa.get_percentage_fraud('Class')}%")
-        # print(f"Percentage of non-frauds present in the dataset: {fa.get_percentage_non_fraud('Class')}%")
+        fa = FraudAnalysis(data=self.data)
+        print(f"Number of rows of the dataset: {fa.get_total_rows()}")
+        print(f"Number of frauds present in the dataset: {fa.get_total_fraud('Class')}")
+        print(f"Number of non-frauds present in the dataset: {fa.get_total_non_fraud('Class')}")
+        print(f"Percentage of frauds present in the dataset: {fa.get_percentage_fraud('Class')}%")
+        print(f"Percentage of non-frauds present in the dataset: {fa.get_percentage_non_fraud('Class')}%")
 
         # Inicia a instância da classe Machine Learning
         model = MachineLearning()
@@ -32,7 +32,7 @@ class Main:
         feature_train, feature_test, target_train, target_test = model.get_split(validator, feature, target)
 
         # Inicializa a árvore de decisão
-        decision_tree_classifier = model.get_decision_tree_classifier()
+        decision_tree_classifier = model.get_decision_tree_classifier(max_depth=10, random_state=0, min_samples_leaf=10)
 
         # Treina o modelo com os dados que recuperamos do split
         decision_tree = model.fit_decision_tree(decision_tree=decision_tree_classifier, feature_train=feature_train,
@@ -40,6 +40,28 @@ class Main:
 
         # Faz a predição para testar o modelo
         target_prediction = model.tree_predict(decision_tree=decision_tree, feature_test=feature_test)
+
+        # Verifica acurácia do modelo
+        print(model.get_tree_score(target_test=target_test, target_predict=target_prediction))
+        print(model.get_confusion_matrix(target_test=target_test, target_predict=target_prediction))
+        print(model.get_precision_score(target_test=target_test, target_predict=target_prediction))
+        print(model.get_recall_score(target_test=target_test, target_predict=target_prediction))
+
+        random_tree_classifier = model.get_random_forest_classifier(n_estimators=50, random_state = 0, max_depth=10)
+        random_forest = model.fit_decision_tree(decision_tree=random_tree_classifier, feature_train=feature_train,
+                                                target_train=target_train)
+        random_forest_prediction = model.tree_predict(decision_tree=random_forest, feature_test=feature_test)
+
+        print(model.get_precision_score(target_test=target_test, target_predict=random_forest_prediction))
+        print(model.get_recall_score(target_test=target_test, target_predict=random_forest_prediction))
+
+        ada_boost_classifier = model.get_ada_boost_classifier(n_estimators=200, random_state = 0)
+        ada_boost = model.fit_decision_tree(decision_tree=ada_boost_classifier, feature_train=feature_train,
+                                            target_train=target_train)
+        ada_boost_prediction = model.tree_predict(decision_tree=ada_boost, feature_test=feature_test)
+
+        print(model.get_precision_score(target_test=target_test, target_predict=ada_boost_prediction))
+        print(model.get_recall_score(target_test=target_test, target_predict=ada_boost_prediction))
 
         # Salva um arquivo .png com a árvore de decisão
         model.save_tree_plot(decision_tree=decision_tree_classifier, name="arquivo1.png", filled=True, fontsize=14)
